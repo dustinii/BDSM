60c3c1d800cafb13874b04217f6e7132dc5e3545
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-05-24 21:10:46 -0700

    Uploaded config, controllers and db

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+// const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS spacemonkey_db;
+CREATE DATABASE spacemonkey_db;
