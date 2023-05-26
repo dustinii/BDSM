1a2537e616d7c238e44191d27b3cf3a16510d777
Paul Koehler <paul.koehler@gmail.com>
2023-05-25 21:25:36 -0700

    added seed files

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const sequelize = require('../config/connection');
 const seedDevelopers = require('./developerData');
+const seedRestaurants = require('./restaurantData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
@@ -9,6 +10,12 @@ const seedAll = async () => {
   console.log('\n----- DEVELOPERS SEEDED -----\n');
 
   // seed additional tables here as needed
+
+  await seedRestaurants();
+  console.log('\n----- RESTAURANTS SEEDED -----\n');
+
+  await seedBurgers();
+  console.log('\n----- BURGERS SEEDED -----\n');
 
   process.exit(0);
 };
