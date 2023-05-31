9b74aac572f915a57aaf05e1fba0a1f3a2055100
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-05-31 13:24:05 -0700

    Update browse handlebars

--- models/index.js
+++ models/index.js
@@ -4,5 +4,13 @@ const Spacemonkey = require('./Spacemonkey');
 const Restaurant = require('./Restaurant');
 const Burger = require('./Burger');
 
+Restaurant.hasMany(Burger, {
+  foreignKey: 'restaurantId'
+});
+
+Burger.belongsTo(Restaurant, {
+  foreignKey: 'restaurantId'
+});
+
 
 module.exports = { User, Developer, Spacemonkey, Restaurant, Burger };
