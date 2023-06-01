68c982c3a1b841bdd1a6ed3a83bca13003c4f88e
dustinii <125335987+dustinii@users.noreply.github.com>
2023-06-01 07:52:17 -0700

    added seeds, images, and css styling, centered card contents

--- seeds/burgerData.js
+++ seeds/burgerData.js
@@ -3,100 +3,148 @@ const { Burger } = require('../models');
 const burgerData = [
   {
     name: 'Corona Burger',
-    image: '',
+    image: '/images/burgers/CoronaBurger.jpg',
     burgerDetails: 'Don\'t think of the beer, the city in the Inland Empire, or that despicable virus. This burger is the crown prince of burgers, as well as the crown jewel of the Solar Flare Café\'s.',
     restaurantId: 1
   },
   {
     name: 'Photosphere Burger',
-    image: '',
+    image: '/images/burgers/PhotosphereBurger.jpg',
     burgerDetails: 'A picture-perfect appearance and a delightfully brilliant taste make for a burger that is a winner for our space monkey delivery team and our esteemed customers as well.',
     restaurantId: 1
   },
   {
-    name: 'Supernova Burger',
-    image: '',
-    burgerDetails: 'How hot is this burger? Think of all of the adjectives you can describe the scale of an explosion, and multiply that by several thousand. The spice explosion in this burger is so big that you\'ll be seeing it in this galaxy and almost every other galaxy in the universe.',
+    name: 'Spicy Supernova Burger',
+    image: '/images/burgers/SpicySupernovaBurger.jpg',
+    burgerDetails: 'Celestial Chicken patty, Supernova spicy sauce, Interstellar iceberg lettuce, Cosmic coleslaw.',
     restaurantId: 1
   },
   {
-    name: 'Ra Burger',
-    image: '',
-    burgerDetails: 'Some people might think we are blasphemous for comparing this burger to a solar deity. Ra disagrees, he says that it would be blasphemous not to eat such a divine gift from the gods!',
+    name: 'Rocket Reuben',
+    image: '/images/burgers/RocketReuben.jpg',
+    burgerDetails: 'Beef patty, Cosmic corned beef, Rocket Russian dressing, Lunar sauerkraut.',    
     restaurantId: 1
   },
   {
     name: 'Escape Velocity Burger',
-    image: '',
+    image: '/images/burgers/EscapeVelocityBurger.jpg',
     burgerDetails: 'What kind of burger is so delicious that you move so quickly in order to get to it? Savory spices and an touch of spice that\'s enough to fuel your escape all the way to the Event Horizon Diner\'s signature burger!',
     restaurantId: 2
   },
   {
-    name: 'No Return Burger',
-    image: '',
-    burgerDetails: 'Eat this burger and you won\'t be returning to that other restaurant!',
+    name: 'Terrestrial Turkey Burger',
+    image: '/images/burgers/TerrestrialTurkey.jpg',
+    burgerDetails: 'Turkey patty, Terrestrial Thousand Island dressing, Interstellar iceberg lettuce, Moon mayo.',
     restaurantId: 2
   },
   {
-    name: 'Time Slip Burger',
-    image: '',
-    burgerDetails: '88 MPH? Slingshot around the sun? A “most excellent” elevator? However you time travel, this burger suits your taste buds no matter the era.',
+    name: 'Alien Avocado Burger',
+    image: '/images/burgers/AlienAvocado.jpg',
+    burgerDetails: 'Vegan patty, Alien avocado, Cosmic coleslaw, Rocket red onions.',
     restaurantId: 2
   },
   {
-    name: 'Gravity Well Burger',
-    image: '',
-    burgerDetails: 'You think you can escape this burger? Gravity disagrees. There\'s so much meat in this burger that you can\'t help but be pulled to it. ',
+    name: 'Big Bang Bison',
+    image: '/images/burgers/BigBangBison.jpg',
+    burgerDetails: 'Bison patty, Big Bang BBQ sauce, Cosmic caramelized onions, Moon Monterey Jack.',
     restaurantId: 2
   },
   {
-    name: 'Exosphere Burger',
-    image: '',
-    burgerDetails: 'Why name of burger for the outermost layer of a heavenly body? It\'s the first thing you notice, and we are confident enough to say that it will probably be the first thing you taste at the Aurora Borealis Bistro.',
+    name: 'Constellation Caprese',
+    image: '/images/burgers/ConstellationCaprese.jpg',
+    burgerDetails: 'Chicken patty, Constellation caprese salad, Mars balsamic glaze, Rocket red onions.',
     restaurantId: 3
   },
   {
-    name: 'Solar Wind Burger',
-    image: '',
-    burgerDetails: 'Wind so powerful that it burns. This also describes the spice in the Solar Wind Burger, which carries all the way from the burger itself to your mouth. Catch it while you can!',
+    name: 'Galactic Greek',
+    image: '/images/burgers/GalacticGreek.jpg',
+    burgerDetails: 'Lamb patty, Feta from a far-off galaxy, Cosmic cucumber, Rocket red onions, Andromeda aioli.',
     restaurantId: 3
   },
   {
-    name: 'Plasmoid Burger',
-    image: '',
-    burgerDetails: 'Like the name “plasmoid” suggests, this burger is no blob. The scientist who coined the term said, “We shall take the liberty of calling this toroidal structure a plasmoid, a word which means plasma-magnetic entity.” We shall take the liberty of calling this one of the best burgers you have ever eaten.',
+    name: 'Space Station Stroganoff',
+    image: '/images/burgers/SpaceStationStroganoff.jpg',
+    burgerDetails: 'Beef patty, Space Station sour cream, Solar sautéed mushrooms, Cosmic caramelized onions.',
     restaurantId: 3
   },
   {
-    name: 'Photon Burger',
-    image: '',
-    burgerDetails: 'This burger makes an impression like a photon torpedo, but is essential to our menu like photons are in the universe. Once you eat this burger, you\'ll know why photons are everywhere and anywhere.',
+    name: 'Starlight Swiss',
+    image: '/images/burgers/StarlightSwiss.jpg',
+    burgerDetails: 'Chicken patty, Starlight Swiss cheese, Cosmic cucumber, Solar Sriracha mayo.',
     restaurantId: 3
   },
   {
-    name: 'EMP Burger',
-    image: '',
-    burgerDetails: 'Make sure you eat an EMP burger before an actual EMP comes and puts our site out of business!',
+    name: 'White Dwarf Wasabi',
+    image: '/images/burgers/WhiteDwarfWasabi.jpg',
+    burgerDetails: 'Tuna patty, White Dwarf wasabi mayo, Cosmic coleslaw, Interstellar iceberg lettuce.',
     restaurantId: 4
   },
   {
-    name: 'Neutron Burger',
-    image: '',
-    burgerDetails: 'A burger with a burst of flavor so strong that it clears out everything else from your mouth and stays there. Trust us, it\'s worth the blast!',
+    name: 'Spacetime Salmon',
+    image: '/images/burgers/SpacetimeSalmon.jpg',
+    burgerDetails: 'Salmon patty, Spacetime soy glaze, Cosmic coleslaw, Orion onions.',
     restaurantId: 4
   },
   {
-    name: 'Big Bang Burger',
-    image: '',
-    burgerDetails: 'The burger at the beginning of all burgers. Created in a nanosecond, and responsible for everything else that came afterwards.',
+    name: 'Lunar Eclispe Lamb',
+    image: '/images/burgers/LunarEclipseLamb.jpg',
+    burgerDetails: 'Moondust-spiced lamb, solar salsa, cosmic cucumber yogurt sauce.',
     restaurantId: 4
   },
   {
-    name: 'Supermassive Burger',
-    image: '',
-    burgerDetails: 'How big is this burger? You remember those challenges at restaurants that give you a large sandwich free if you eat it in a certain amount of time? Finish this burger in an hour and the rest of your orders for a week are on us. It\'s *that* big.',
+    name: 'Neptune Noodle Burger',
+    image: '/images/burgers/NeptuneNoodleBurger.jpg',
+    burgerDetails: 'Noodle "bun", solar-seared tofu, lunar lotus root, Andromeda Asian slaw.',
     restaurantId: 4
   },
+  {
+    name:'Galaxy Glider',
+    image: '/images/burgers/GalaxyGlider.jpg',
+    burgerDetails: 'Beef patty, galaxy blue cheese, caramelized Milky Way onions, Saturn onion rings, Mars-malade sauce.',
+    restaurantId: 5
+  },
+  {
+    name:'Nebula Nacho Burger',
+    image: '/images/burgers/NebulaNachoBurger.jpg',
+    burgerDetails: 'Black bean burger, Nebula nacho cheese, cosmic corn salsa, jalapenos.',
+    restaurantId: 5
+  },
+  {
+    name:'Comet Crunch',
+    image: '/images/burgers/CometCrunch.jpg',
+    burgerDetails: 'Chicken burger, Comet\'s crispy lettuce, spicy solar slaw, moon mustard.',
+    restaurantId: 5
+  },
+  {
+    name:'Pulsar Pulled Pork',
+    image: '/images/burgers/PulsarPulledPork.jpg',
+    burgerDetails: 'Pulled pork, coleslaw, Pulsar pickle, Andromeda Aioli.',
+    restaurantId: 5
+  },
+  {
+    name: 'Uranus Ultimate Umami Burger',
+    image: '/images/burgers/UranusUltimateUmamiBurger.jpg',
+    burgerDetails: 'Beef patty, Solar shiitake mushrooms, Venus\'s vintage cheddar, Cosmic caramelized onions.',
+    restaurantId: 6
+  },
+  {
+    name: 'Pluto\'s Pork Burger',
+    image: '/images/burgers/PlutosPorkBurger.jpg',
+    burgerDetails: 'Pluto pork patty, asteroid apple slaw, cosmic cheddar, black hole BBQ sauce.',
+    restaurantId: 6
+  },
+  {
+    name: 'Andromeda Ahi Tuna',
+    image: '/images/burgers/AndromedaAhiTuna.jpg',
+    burgerDetails: 'Nebula-seared tuna, cosmic coleslaw, celestial caper mayo.',
+    restaurantId: 6
+  },
+  {
+    name: 'Cosmic Crab Cake',
+    image: '/images/burgers/CosmicCrabCake.jpg',
+    burgerDetails: 'Star-shaped crab cakes, cosmic coleslaw, Venusian vegetable tartar sauce.',
+    restaurantId: 6
+  }
 ];
 
 const seedBurgers = () => Burger.bulkCreate(burgerData);
--- seeds/restaurantData.js
+++ seeds/restaurantData.js
@@ -3,24 +3,34 @@ const { Restaurant } = require('../models');
 const restaurantData = [
   {
     name: 'Solar Flare Café',
-    image: '',
+    image: '/images/restaurants/SolarFlareCafe.jpg',
     description: 'A fully stocked café with a menu as bright as the sun! However, you can (and are in fact, encouraged) to look directly at our café.',
   },
   {
     name: 'Event Horizon Diner',
-    image: '',
+    image: '/images/restaurants/EventHorizonDiner.jpg',
     description: 'Be careful when you come to this diner. Once you cross the event horizon, you may never be able to go back!',
   },
   {
     name: 'Aurora Borealis Bistro',
-    image: '',
+    image: '/images/restaurants/AuroraBorealisBistro.jpg',
     description: 'Bistros have never looked this good! Well, some have, but we are very confident that this one shines like the northern lights.',
   },
   {
     name: 'Black Hole Desserts',
-    image: '',
+    image: '/images/restaurants/BlackHoleDesserts.jpg',
     description: 'The name may be intimidating, but think of it this way... there is no way you can ignore our menu!',
   },
+  {
+    name: 'Stargrazers Grille',
+    image: '/images/restaurants/StarGrazersGrille.jpg',
+    description: 'At the heart of the Milky Way, StarGrazers Grille is a one-stop cosmic feast. Here, earthly desires meet celestial delights to create an unforgettable gastronomic experience.'
+  },
+  {
+    name: 'Galactic Grub',
+    image: '/images/restaurants/GalacticGrub.jpg',
+    description: 'Orbiting around Saturn\'s rings, the Galactic Grillhouse offers interplanetary patrons a taste of the infinite, with burgers inspired by galaxies far, far away.'
+  }
 ];
 
 const seedRestaurants = () => Restaurant.bulkCreate(restaurantData);
